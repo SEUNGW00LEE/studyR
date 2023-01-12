7e2da9576081aefcbc5978f47f3c47669775084e
seungwoo <seungwoolee99@gmail.com>
2023-01-12 15:28:23 +0900

    complete 01.12

---
+++ fileR/mergeData.R
@@ -0,0 +1,10 @@
+#중간고사 데이터
+test1 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    midterm = c(60, 80, 70, 90, 85))
+#기말고사 데이터
+test2 <- data.frame(id = c(1, 2, 3, 4, 5),
+                    midterm = c(75, 70, 650, 88, 90))
+
+test1
+
+test2
